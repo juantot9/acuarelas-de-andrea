203d37a75f5cdb0cbb7204158ca457106c4af294
Wanan <juaaratri@gmail.com>
2026-05-02 00:12:48 +0200

    fix images and add SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+<<<<<<< HEAD
   adapter: cloudflare()
 });
+=======
+  site: 'https://acuarelasdeandrea.com',
+  image: {
+    service: sharpImageService(),
+  },
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/aviso-legal/') && !page.includes('/privacidad/'),
+    }),
+  ],
+});
+>>>>>>> b74fba4 (fix images and add SEO improvements)
